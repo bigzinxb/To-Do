f15f605f0e13c2abce69b7489ebda845c970d8d1
bigzinxb <bigzin369@gmail.com>
2023-06-20 22:00:29 -0300

    crudd

--- db/tasks.rb
+++ db/tasks.rb
@@ -1,8 +1,11 @@
+require 'sqlite3'
+
 class TasksDataBase
     attr_reader :db
 
     def initialize
         @db = SQLite3::Database.new  "tasks.db"
+        self.create_table
     end
 
     def create_task(title, comment)
@@ -14,15 +17,20 @@ class TasksDataBase
     end
 
     def update_task(title, comment)
-        @db.execute("UPDATE Tasks SET title = ", title , ", comment = ", comment)
+        @db.execute("UPDATE Tasks SET title_task = ?, comment_task = ?", title, comment)
     end
 
-    def delete_tasks(title, comment)
-        @db.execute("DELETE FROM Tasks WHERE title IS ", title , "AND comment IS ", comment)
+    def delete_task(title, comment)
+        @db.execute("DELETE FROM Tasks WHERE title_task IS ? AND comment_task IS ?", title, comment)
     end
 
     private
-
     def create_table
+        @db.execute("CREATE TABLE 'Tasks' (
+            'id_task'	INTEGER,
+            'title_task'	TEXT,
+            'comment_task'	TEXT,
+            PRIMARY KEY('id_task' AUTOINCREMENT)
+        );")
     end
 end
