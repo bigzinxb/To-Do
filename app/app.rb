aa02615f224098454afdac8465946a817e62205e
bigzinxb <bigzin369@gmail.com>
2023-06-23 22:28:15 -0300

    crud

--- app/app.rb
+++ app/app.rb
@@ -1,14 +1,12 @@
 require_relative '../db/tasks.rb'
 require_relative 'sanitize/sanitize.rb'
 
-class Tasks
+class Tasks < FunctionsDatabase
     include Sanitize
-    include TasksDataBase
 
     def create_task(title, comment)
         if Sanitize.sanitize_input(title) != nil && Sanitize.sanitize_input(comment) != nil
-            query_tasks = TasksDataBase::FunctionsTask.new
-            query_tasks.insert_task(title, comment)
+            #querys
         end
 
         puts "Ta tentando SQLI seu poha"
--- db/tasks.rb
+++ db/tasks.rb
@@ -1,40 +1,36 @@
 require 'sqlite3'
 
-module TasksDataBase
+class FunctionsDatabase
+    attr_reader :db
 
-    class FunctionsTask
+    def initialize
+        @db = SQLite3::Database.new "tasks.db"
+        self.create_table
+    end
 
-        attr_reader :db
+    def create_tasks(title, comment)
+        @db.execute("INSERT INTO Tasks VALUES (null, ?, ?)", title, comment)
+    end
 
-        def self.initialize
-            @db = SQLite3::Database.new  "tasks.db"
-            self.create_table
-        end
-        
-        def self.insert_task(title, comment)
-            @db.execute("INSERT INTO Tasks VALUES (null,?,?)", title, comment)
-        end
+    def read_tasks
+        @db.execute("SELECT * FROM Tasks")
+    end
 
-        def self.read_task
-            @db.execute("SELECT * FROM Tasks")
-        end
+    def update_tasks(title_update, comment_update, id_task)
+        @db.execute("UPDATE Tasks SET title_task = ?, comment_task = ? WHERE id_task = ?", title_update, comment_update, id_task)
+    end
 
-        def self.update_task(title, comment)
-            @db.execute("UPDATE Tasks SET title_task = ?, comment_task = ?", title, comment)
-        end
+    def delete_tasks(id_task)
+        @db.execute("DELETE FROM Tasks WHERE id_task = ?", id_task)
+    end
 
-        def self.delete_task(title, comment)
-            @db.execute("DELETE FROM Tasks WHERE title_task IS ? AND comment_task IS ?", title, comment)
-        end
-
-        private
-        def self.create_table
-            @db.execute("CREATE TABLE 'Tasks' (
-                'id_task'	INTEGER,
-                'title_task'	TEXT,
-                'comment_task'	TEXT,
-                PRIMARY KEY('id_task' AUTOINCREMENT)
-            );")
-        end
+    private
+    def create_table
+        @db.execute("CREATE TABLE 'Tasks' (
+            'id_task'	INTEGER,
+            'title_task'	TEXT,
+            'comment_task'	TEXT,
+            PRIMARY KEY('id_task' AUTOINCREMENT)
+        );")
     end
 end
